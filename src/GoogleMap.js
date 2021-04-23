13ae897d9f853c16e6c791f1de8e0b71e50001af
Akhil <33389278+iamakhil9@users.noreply.github.com>
2021-04-23 18:17:53 +0530

    Update GoogleMap.js
    create .env file  with REACT_APP_API_KEY = "your  google map api key"

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -56,5 +56,5 @@ export class MapContainer extends Component {
   }
 
   export default GoogleApiWrapper({
-    apiKey: process.env.REACT_APP_API_KEY
+    apiKey: process.env.REACT_APP_API_KEY 
   })(MapContainer)
